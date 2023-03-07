308855bc035a56647db0228a4f1c5bc57c535249
kevin-liu <liu.kevin1997@gmail.com>
2023-03-07 12:49:05 -0500

    finished

--- server.js
+++ server.js
@@ -4,8 +4,28 @@ const {Musician} = require("./Musician")
 const {sequelize} = require("./db")
 
 const port = 3000;
+app.use(express.json())
 
 //TODO
+app.post('/musicians', async(req, res) => {
+    const newMusician = Musician.build({
+        name: req.body.name,
+        instrument: req.body.instrument
+    })
+
+    await newMusician.save();
+})
+app.delete('/musicians/:id', async(req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id)
+    await restaurant.destroy()
+})
+
+app.put('/musicians/:id', async(req, res) => {
+    const data = await Musician.findByPk(req.params.id)
+
+    await data.update(req.body)
+
+})
 
 app.get("/musicians" , async (req, res) => {
     const data = await Musician.findAll();
