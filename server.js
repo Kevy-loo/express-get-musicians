92734950a2538a29d20c4c252c7aed0984c7b6ce
kevin-liu <liu.kevin1997@gmail.com>
2023-03-08 13:23:55 -0500

    finished

--- server.js
+++ server.js
@@ -2,41 +2,15 @@ const express = require("express");
 const app = express();
 const {Musician} = require("./Musician")
 const {sequelize} = require("./db")
+const musicRouter = require('./router/music');
+
+
 
 const port = 3000;
 app.use(express.json())
-
+app.use("/musicians", musicRouter)
 //TODO
-app.post('/musicians', async(req, res) => {
-    const newMusician = Musician.build({
-        name: req.body.name,
-        instrument: req.body.instrument
-    })
 
-    await newMusician.save();
-})
-app.delete('/musicians/:id', async(req, res) => {
-    const restaurant = await Restaurant.findByPk(req.params.id)
-    await restaurant.destroy()
-})
-
-app.put('/musicians/:id', async(req, res) => {
-    const data = await Musician.findByPk(req.params.id)
-
-    await data.update(req.body)
-
-})
-
-app.get("/musicians" , async (req, res) => {
-    const data = await Musician.findAll();
-    return res.json(data) 
-})
-
-app.get("/musicians/:id", async (req, res) => {
-    const id = req.params.id
-    const data = await Musician.findByPk(id);
-    res.json(data)
-})
 
 
 app.listen(port, () => {
